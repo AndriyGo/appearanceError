5c382111a49930971e1d6af28c993f7adcf401f4
Andriy Gordiychuk <andriy@glenmax.com>
2018-09-26 16:22:57 +0300

    Initial Commit

---
+++ appearanceError/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appearanceError
+//
+//  Created by Andriy Gordiychuk on 26/09/2018.
+//  Copyright © 2018 Glenmax Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
