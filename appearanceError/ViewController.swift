cc4afe9452b55672a39f32fb786cd6dfd9927cc4
Andriy Gordiychuk <andriy@glenmax.com>
2018-09-26 16:30:47 +0300

    Initial commit

--- appearanceError/ViewController.swift
+++ appearanceError/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.font = UIFont.systemFont(ofSize: 22)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
